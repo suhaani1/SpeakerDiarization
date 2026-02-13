ca73d862a17b4a38279b02dc307e6113db47ed6a
suhani kumari <suhanimehta.anu@gmail.com>
2026-02-13 21:08:21 +0530

    Clean up audio_utils.py by removing unused code
    Removed commented-out code for audio conversion using torchaudio.

--- audio_utils.py
+++ audio_utils.py
@@ -1,20 +1,4 @@
-# import torchaudio
 
-
-# def convert_audio_to_16k_mono(input_path, output_path):
-#     waveform, sample_rate = torchaudio.load(input_path)
-
-#     if waveform.shape[0] > 1:
-#         waveform = waveform.mean(dim=0, keepdim=True)
-
-#     if sample_rate != 16000:
-#         resampler = torchaudio.transforms.Resample(
-#             orig_freq=sample_rate,
-#             new_freq=16000
-#         )
-#         waveform = resampler(waveform)
-
-#     torchaudio.save(output_path, waveform, 16000)
 import os
 os.environ["PATH"] += os.pathsep + r"D:\SHIVANI\INTERNSHIP\CDAC\cheat\ffmpeg-8.0.1-essentials_build\ffmpeg-8.0.1-essentials_build\bin"
 
